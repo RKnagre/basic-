b040f3ab5f1a3c5bcf339215ae7141127e27ce37
ajay <nagre7779@gmail.com>
2026-07-06 10:18:10 +0530

    First Project Commit

---
+++ baisc.py
@@ -0,0 +1,49 @@
+"""
+#Simple Interest!...
+p=int(input("Principal Amount: "))
+r=int(input("Rate of interest: "))
+t=int(input("Times in (Years): "))
+
+interest=(p*r*t)/100
+print("Simple Interest= ",interest)
+"""
+#Area of circle
+"""
+radius=int(input("Enter radius: "))
+area=3.14*radius*radius
+print("area of circle: ",area)
+
+#Area of square
+side=int(input("Enter Sides of Sqaure: "))
+area=side*side
+print("Area of Square: ",area)
+
+# Area of Rectangle
+length=int(input("Enter Length:"))
+width=int(input("Enter Width: "))
+area=length*width
+print("area of rectangle: ",area)
+
+#Area of Triangle
+base=int(input("Enter the base : "))
+height=int(input("Enter the height: "))
+area=0.5*base*height
+print("Area of triangle= ",area)
+
+#Average!...
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+
+average=(a+b+c)/3
+print("Average of numbers= ",average)
+"""
+#find the percentage from marks of student!...
+english=int(input("English marks: "))
+maths=int(input("Maths marks: "))
+science=int(input("Science marks: "))
+gujarati=int(input("gujarati marks: "))
+total=english+maths+science+gujarati
+percentage=(total/400)*100
+print("Total marks= ",total)
+print("Percentage= ",percentage ,"%")
