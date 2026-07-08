49965779fb11aeec2cd6687b1b1e38526a6c2447
atul <nagre7779@gmail.com>
2026-07-08 13:42:43 +0530

    Pycharm Project

--- if.py
+++ if.py
@@ -1,27 +1,42 @@
-#If else statement!...
 """
+a=b=c=30
+print(a)
+print(b)
+print(c)
+
+a,b,c=10,20.5,"hello"
+print(a)
+print(b)
+print(c)
+
+#If statement programs
 num=int(input("Enter number: "))
-if(num%2==0):
-    print(num,"is Even")
+if(num%2==0 and num>0):
+    print(num,"positive")
+    print("even")
+elif(num<0):
+    print(num,"is negative")
 else:
-    print(num,"is odd")
+    #print("Zero")
+    print("odd")
 
 username=input("Enter username: ")
-password=int(input("Enter Password: "))
-
-if(username=="admin" and password==1234):
-    print("Welcome,Login Successfully")
+password=int(input("Enter password: "))
+if(username=="Atul" and password==7480):
+    print("Welcome, Login Successfully!...")
 else:
-    print("Please enter valid password")
+    print("Please enter valid credentials")
 """
 marks=int(input("Enter student marks: "))
-if(marks<0 and marks>100):
-    print("Invalid")
-elif(marks>80):
-    print("first class")
-elif(marks>50):
+if(marks>100):
+    print("Invalid Input")
+elif(marks>=90):
+    print("Distinction")
+elif(marks>=75):
     print("Second Class")
-elif(marks>35):
+elif(marks>=50):
     print("Pass")
+elif(marks>=0):
+    print("fail")
 else:
-    print("Fail")
+    print("invalid marks")
